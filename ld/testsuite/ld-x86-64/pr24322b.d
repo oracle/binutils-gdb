b3feab1947edecbf65e65d0d9e393ce4ccec8df4
David Faust <david.faust@oracle.com>
2020-11-23 08:32:43 -0800

    binutils-x86-IBT-and-missing-notes.patch

---
+++ ld/testsuite/ld-x86-64/pr24322b.d
@@ -0,0 +1,10 @@
+#source: pr24322c.s
+#source: pr24322b.s
+#as: --64 -defsym __64_bit__=1
+#ld: -z shstk -melf_x86_64
+#readelf: -n
+
+Displaying notes found in: .note.gnu.property
+  Owner                 Data size	Description
+  GNU                  0x000000..	NT_GNU_PROPERTY_TYPE_0
+      Properties: x86 feature: SHSTK
