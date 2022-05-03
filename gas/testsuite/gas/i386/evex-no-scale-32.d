84b547033624785f4cc7a17183073699a367c4f9
David Faust <david.faust@oracle.com>
2022-05-03 09:51:38 -0700

    binutils-x86-tpause.patch

--- gas/testsuite/gas/i386/evex-no-scale-32.d
+++ gas/testsuite/gas/i386/evex-no-scale-32.d
@@ -1,4 +1,3 @@
-#source: evex-no-scale.s
 #objdump: -dw
 #name: ix86 EVEX no disp scaling
 
@@ -11,4 +10,3 @@ Disassembly of section .text:
  +[a-f0-9]+:	62 f1 7c 48 28 04 25 40 00 00 00 	vmovaps 0x40\(,%eiz,1\),%zmm0
  +[a-f0-9]+:	62 f1 7c 48 28 05 40 00 00 00 	vmovaps 0x40,%zmm0
  +[a-f0-9]+:	67 62 f1 7c 48 28 06 40 00 	vmovaps 0x40,%zmm0
-#pass
