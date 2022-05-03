f87d10cb821a9ccfd45396d6b5d2b3789d4e24f5
David Faust <david.faust@oracle.com>
2022-05-03 09:51:20 -0700

    binutils-fix-testsuite-failures.patch

--- ld/testsuite/ld-size/size-6a.c
+++ ld/testsuite/ld-size/size-6a.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-extern char bar[];
-extern char size_of_bar asm ("bar@SIZE");
-extern void set_bar (int, int);
+extern char  bar[];
+extern char  size_of_bar asm ("bar@SIZE");
+extern void  set_bar (int, int);
+char *       bar_size = & size_of_bar;
 
 int
-main ()
+main (void)
 {
   set_bar (1, 20);
-  if (10 == (long) &size_of_bar && bar[1] == 20)
+  if (10 == (long) bar_size && bar[1] == 20)
     printf ("OK\n");
 
   return 0;
