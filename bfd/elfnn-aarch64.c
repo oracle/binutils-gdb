52a0360b46e0acd1fb38c73f93a1f78280c7a7db
David Faust <david.faust@oracle.com>
2022-06-09 08:44:45 -0700

    binutils-aarch64-ifunc-relocations.patch

--- bfd/elfnn-aarch64.c
+++ bfd/elfnn-aarch64.c
@@ -4959,9 +4959,27 @@ elfNN_aarch64_final_link_relocate (reloc_howto_type *howto,
       const char *name;
       bfd_vma addend = 0;
 
-      if ((input_section->flags & SEC_ALLOC) == 0
-	  || h->plt.offset == (bfd_vma) -1)
-	abort ();
+      if ((input_section->flags & SEC_ALLOC) == 0)
+        {
+          /* Dynamic relocs are not propagated for SEC_DEBUGGING
+             sections because such sections are not SEC_ALLOC and
+             thus ld.so will not process them.  */
+          if ((input_section->flags & SEC_DEBUGGING) != 0)
+            return bfd_reloc_ok;
+
+          if (h->root.root.string)
+            name = h->root.root.string;
+          else
+            name = bfd_elf_sym_name (input_bfd, symtab_hdr, sym, NULL);
+          _bfd_error_handler
+            /* xgettext:c-format */
+            (_("%B(%A+%#Lx): unresolvable %s relocation against symbol `%s'"),
+              input_bfd, input_section, rel->r_offset, howto->name, name);
+          bfd_set_error (bfd_error_bad_value);
+          return FALSE;
+        }
+      else if (h->plt.offset == (bfd_vma) -1)
+        goto bad_ifunc_reloc;
 
       /* STT_GNU_IFUNC symbol must go through PLT.  */
       plt = globals->root.splt ? globals->root.splt : globals->root.iplt;
@@ -4970,6 +4988,7 @@ elfNN_aarch64_final_link_relocate (reloc_howto_type *howto,
       switch (bfd_r_type)
 	{
 	default:
+bad_ifunc_reloc:
 	  if (h->root.root.string)
 	    name = h->root.root.string;
 	  else
@@ -7605,8 +7624,39 @@ elfNN_aarch64_reloc_type_class (const struct bfd_link_info *info ATTRIBUTE_UNUSE
 				const asection *rel_sec ATTRIBUTE_UNUSED,
 				const Elf_Internal_Rela *rela)
 {
+  struct elf_aarch64_link_hash_table *htab = elf_aarch64_hash_table (info);
+
+  if (htab->root.dynsym != NULL
+      && htab->root.dynsym->contents != NULL)
+    {
+      /* Check relocation against STT_GNU_IFUNC symbol if there are
+        dynamic symbols.  */
+      bfd *abfd = info->output_bfd;
+      const struct elf_backend_data *bed = get_elf_backend_data (abfd);
+      unsigned long r_symndx = ELFNN_R_SYM (rela->r_info);
+      if (r_symndx != STN_UNDEF)
+       {
+         Elf_Internal_Sym sym;
+         if (!bed->s->swap_symbol_in (abfd,
+                                      (htab->root.dynsym->contents
+                                       + r_symndx * bed->s->sizeof_sym),
+                                      0, &sym))
+           {
+             /* xgettext:c-format */
+             _bfd_error_handler (_("%B symbol number %lu references"
+                                   " nonexistent SHT_SYMTAB_SHNDX section"),
+                                   abfd, r_symndx);
+             /* Ideally an error class should be returned here.  */
+           }
+         else if (ELF_ST_TYPE (sym.st_info) == STT_GNU_IFUNC)
+           return reloc_class_ifunc;
+       }
+    }
+
   switch ((int) ELFNN_R_TYPE (rela->r_info))
     {
+    case AARCH64_R (IRELATIVE):
+      return reloc_class_ifunc;
     case AARCH64_R (RELATIVE):
       return reloc_class_relative;
     case AARCH64_R (JUMP_SLOT):
