19ebbc95fbe027564ee521d1eb136b16cd2a2990
Cupertino Miranda <cupertino.miranda@oracle.com>
2022-11-30 16:41:48 +0000

    69 - binutils-s390-ld-test-fixes.patch

--- ld/testsuite/ld-plugin/plugin-12.d
+++ ld/testsuite/ld-plugin/plugin-12.d
@@ -1,5 +1,5 @@
 #...
-.*: symbol `.*unc' definition: 0, visibility: 0, resolution: 2
+.*: symbol `.*unc' definition: 0, visibility: 0, resolution: .
 .*: symbol `.*unc1' definition: 0, visibility: 1, resolution: 3
 .*: symbol `.*unc2' definition: 0, visibility: 2, resolution: 3
 .*: symbol `.*unc3' definition: 0, visibility: 3, resolution: 3
