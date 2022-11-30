bd9a108d7a4040a7caca50890cf6ea782c94536e
Cupertino Miranda <cupertino.miranda@oracle.com>
2022-11-30 16:42:03 +0000

    100 - binutils-s390-z16.patch

--- opcodes/s390-mkopc.c
+++ opcodes/s390-mkopc.c
@@ -377,9 +377,11 @@ main (void)
       else if (strcmp (cpu_string, "z14") == 0
 	       || strcmp (cpu_string, "arch12") == 0)
 	min_cpu = S390_OPCODE_ARCH12;
-      else if (strcmp (cpu_string, "arch13") == 0)
+      else if ((strcmp (cpu_string, "z15") == 0
+		|| strcmp (cpu_string, "arch13") == 0))
 	min_cpu = S390_OPCODE_ARCH13;
-      else if (strcmp (cpu_string, "arch14") == 0)
+      else if ((strcmp (cpu_string, "z16") == 0
+		|| strcmp (cpu_string, "arch14") == 0))
 	min_cpu = S390_OPCODE_ARCH14;
       else {
 	fprintf (stderr, "Couldn't parse cpu string %s\n", cpu_string);
