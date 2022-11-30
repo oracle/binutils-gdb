5f77880e75cd1fcd3f81c156e683cd405bb09cc2
Cupertino Miranda <cupertino.miranda@oracle.com>
2022-11-30 16:41:54 +0000

    81 - binutils-x86-tpause.patch

--- ld/testsuite/ld-x86-64/property-x86-ibt3a.d
+++ ld/testsuite/ld-x86-64/property-x86-ibt3a.d
@@ -1,12 +1,10 @@
 #source: property-x86-3.s
 #source: property-x86-ibt.s
-#as: --64 -defsym __64_bit__=1 -mx86-used-note=yes
+#as: --64 -defsym __64_bit__=1
 #ld: -r -melf_x86_64
 #readelf: -n
 
 Displaying notes found in: .note.gnu.property
   Owner                 Data size	Description
-  GNU                  0x00000030	NT_GNU_PROPERTY_TYPE_0
+  GNU                  0x00000010	NT_GNU_PROPERTY_TYPE_0
       Properties: x86 ISA needed: i486, 586, SSE2, SSE3
-	x86 ISA used: <None>
-	x86 feature used: x86
--- ld/testsuite/ld-x86-64/property-x86-shstk3b-x32.d
+++ ld/testsuite/ld-x86-64/property-x86-shstk3b-x32.d
@@ -1,12 +1,10 @@
 #source: property-x86-shstk.s
 #source: property-x86-3.s
-#as: --x32 -mx86-used-note=yes
+#as: --x32
 #ld: -r -m elf32_x86_64
 #readelf: -n
 
 Displaying notes found in: .note.gnu.property
   Owner                 Data size	Description
-  GNU                  0x00000024	NT_GNU_PROPERTY_TYPE_0
+  GNU                  0x0000000c	NT_GNU_PROPERTY_TYPE_0
       Properties: x86 ISA needed: i486, 586, SSE2, SSE3
-	x86 ISA used: <None>
-	x86 feature used: x86
