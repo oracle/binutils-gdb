0fac0528aec79fc5716e0e05db7394469452a213
Cupertino Miranda <cupertino.miranda@oracle.com>
2023-07-26 14:18:29 +0000

    063 - gdb-moribund-utrace-workaround.patch

--- gdb/breakpoint.c
+++ gdb/breakpoint.c
@@ -12016,6 +12016,8 @@ update_global_location_list (enum ugll_insert_mode insert_mode)
 		 traps we can no longer explain.  */
 
 	      old_loc->events_till_retirement = 3 * (thread_count () + 1);
+	      /* Red Hat Bug 590623.  */
+	      old_loc->events_till_retirement *= 10;
 	      old_loc->owner = NULL;
 
 	      VEC_safe_push (bp_location_p, moribund_locations, old_loc);
