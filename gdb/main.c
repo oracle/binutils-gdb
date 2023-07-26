5d4f324c5e750da7825d761ed3372f29a2b038aa
Cupertino Miranda <cupertino.miranda@oracle.com>
2023-07-26 14:18:52 +0000

    105 - gdb-bz1219747-attach-kills.patch

--- gdb/main.c
+++ gdb/main.c
@@ -1115,7 +1115,10 @@ captured_main_1 (struct captured_main_args *context)
       if (isdigit (pid_or_core_arg[0]))
 	{
 	  if (catch_command_errors (attach_command, pid_or_core_arg,
-				    !batch_flag) == 0)
+				    !batch_flag) == 0
+	      /* attach_command could succeed partially and core_file_command
+		 would try to kill it.  */
+	      && !have_inferiors ())
 	    catch_command_errors (core_file_command, pid_or_core_arg,
 				  !batch_flag);
 	}
