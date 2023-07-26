11e5e01ad222286b9989fd963cc273054a08716a
Cupertino Miranda <cupertino.miranda@oracle.com>
2023-07-26 14:18:04 +0000

    011 - gdb-6.3-test-movedir-20050125.patch

---
+++ gdb/testsuite/gdb.base/move-dir.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "move-dir.h"
+
+int main() {
+   const char* hw = "hello world.";
+   printf ("%s\n", hw);;
+   other();
+}
+
---
+++ gdb/testsuite/gdb.base/move-dir.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+void other() {
+  const char* ostring = "other";
+  printf ("%s\n", ostring);;
+}
+
